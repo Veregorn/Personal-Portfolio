6a043adf8def1a10cd5b1ece5438613f075f4045
Veregorn <rjbarco@gmail.com>
2023-09-01 16:12:48 +0200

    Added some random content to ensure everything works right

---
+++ index.bundle.js
@@ -0,0 +1,21 @@
+(self["webpackChunkpersonal_portfolio"] = self["webpackChunkpersonal_portfolio"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+const randomTextElement = document.createElement("p");
+randomTextElement.textContent = "Random text";
+document.body.appendChild(randomTextElement);
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEsTUFBTUEsaUJBQWlCLEdBQUdDLFFBQVEsQ0FBQ0MsYUFBYSxDQUFDLEdBQUcsQ0FBQztBQUNyREYsaUJBQWlCLENBQUNHLFdBQVcsR0FBRyxhQUFhO0FBQzdDRixRQUFRLENBQUNHLElBQUksQ0FBQ0MsV0FBVyxDQUFDTCxpQkFBaUIsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3BlcnNvbmFsLXBvcnRmb2xpby8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCByYW5kb21UZXh0RWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoXCJwXCIpO1xucmFuZG9tVGV4dEVsZW1lbnQudGV4dENvbnRlbnQgPSBcIlJhbmRvbSB0ZXh0XCI7XG5kb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKHJhbmRvbVRleHRFbGVtZW50KTsiXSwibmFtZXMiOlsicmFuZG9tVGV4dEVsZW1lbnQiLCJkb2N1bWVudCIsImNyZWF0ZUVsZW1lbnQiLCJ0ZXh0Q29udGVudCIsImJvZHkiLCJhcHBlbmRDaGlsZCJdLCJzb3VyY2VSb290IjoiIn0=
