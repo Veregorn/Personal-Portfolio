c72a37f15f86e2cccc473f662a948f2539e7e4ce
Veregorn <rjbarco@gmail.com>
2023-09-20 14:41:45 +0200

    Minor responsive changes

---
+++ src_assets_images_17F96B97-9B2E-426B-A68B-6936D6FDD12E_jpg.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkpersonal_portfolio"] = self["webpackChunkpersonal_portfolio"] || []).push([["src_assets_images_17F96B97-9B2E-426B-A68B-6936D6FDD12E_jpg"],{
+
+/***/ "./src/assets/images/17F96B97-9B2E-426B-A68B-6936D6FDD12E.jpg":
+/*!********************************************************************!*\
+  !*** ./src/assets/images/17F96B97-9B2E-426B-A68B-6936D6FDD12E.jpg ***!
+  \********************************************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "fa6e18c599bb0b922c3a.jpg";
+
+/***/ })
+
+}]);
---
+++ src_assets_images_project3_png.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+(self["webpackChunkpersonal_portfolio"] = self["webpackChunkpersonal_portfolio"] || []).push([["src_assets_images_project3_png"],{
+
+/***/ "./src/assets/images/project3.png":
+/*!****************************************!*\
+  !*** ./src/assets/images/project3.png ***!
+  \****************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "e9dadc9691b1923d3f01.png";
+
+/***/ })
+
+}]);
